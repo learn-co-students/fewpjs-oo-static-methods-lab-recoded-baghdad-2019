ce5cb8630facd6aee3eed2fc8140f0f26500d48d
K3be <ahmed.13.alkaaby@gmail.com>
2019-11-29 13:40:19 +0000

    Done.

--- index.js
+++ index.js
@@ -1,3 +1,26 @@
 class Formatter {
-  //add static methods here
+  static capitalize(str){
+      return str.charAt(0).toUpperCase()+str.slice(1);
+    }
+    static sanitize(str){
+      return str.replace(/[^A-Za-z0-9 '-]/g, '')
+    }
+    static titleize(str){
+    let exceptions=['the','a','an','but','of','and','for','at','by','from'];
+    let newArr=str.split(' ');
+    let newArr2=[];
+    for(let i=0; i<newArr.length; i++){
+      if(i === 0){
+       newArr2.push(this.capitalize(newArr[i]));
+      }
+      else{
+      if(exceptions.includes(newArr[i])){
+         newArr2.push(newArr[i])
+       }
+       else {
+         newArr2.push(this.capitalize(newArr[i]))
+       }}
+}
+return newArr2.join(' ');
+}
 }
